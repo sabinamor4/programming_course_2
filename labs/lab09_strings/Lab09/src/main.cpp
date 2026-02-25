5d19069bdd0e7e1923e845eff67aafb8670d182e
sabinamor4 <sabinamor4@gmail.com>
2026-02-25 13:36:53 +0700

    test /N

--- labs/lab09_strings/Lab09/src/main.cpp
+++ labs/lab09_strings/Lab09/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 // Lab 09
 // TODO: реализуйте решение по заданию в labs/lab09_strings/README.md
@@ -14,19 +15,21 @@ int main() {
     // TODO: добавьте примеры и вывод результатов.
 
     // Упражнение 4: ввод количества слов и самих слов.
-    int word_count = 0;
-    if (!(std::cin >> word_count)) {
-        return 0;
-    }
+    //int word_count = 0;
+    //if (!(std::cin >> word_count)) {
+    //    return 0;
+    //}
 
-    std::vector<std::string> words;
-    words.reserve(word_count);
-    for (int i = 0; i < word_count; ++i) {
-        std::string word;
-        std::cin >> word;
-        words.push_back(word);
-    }
+    //std::vector<std::string> words;
+    //words.reserve(word_count);
+    //for (int i = 0; i < word_count; ++i) {
+    //    std::string word;
+    //    std::cin >> word;
+    //    words.push_back(word);
+    //}
 
-    // TODO: выведите слова с четными номерами (2, 4, 6, ...), по одному в строке.
-    return 0;
+    //// TODO: выведите слова с четными номерами (2, 4, 6, ...), по одному в строке.
+    //return 0;
+
+    cout << "Char represented by \\101: " << '\101' << endl;
 }
