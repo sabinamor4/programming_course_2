d6f3348f1110ebb88fd05cd51c2a080ff40ed7ed
sabinamor4 <sabinamor4@gmail.com>
2026-03-04 23:37:44 +0700

    int main() и часть pack()

--- labs/lab09_strings/Lab09/src/main.cpp
+++ labs/lab09_strings/Lab09/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 
@@ -10,21 +11,37 @@ using namespace std;
 // - без лишнего текста
 // - числа через пробел
 // - если несколько строк — в фиксированном порядке
+
 int main() {
     // Упражнения 1–3: демонстрация/анализ строковых функций (без ввода).
+    cout << "happy!!!";
+    char* s1 = new char[20];
+    cout << strcpy(s1, "smile!!!");
+    char *s2;
+    s2 = new char[20];
+    //getc(s2);
+    //cin.getline(s2, 6, ' ');
+    //puts(s1);
+    cin >> s2;
+
+    cout << strcpy(s1, "happy!!!");
+    cout << strncat(s1, s2, 3);
+    cout << strlen(s1);
+
     // TODO: добавьте примеры и вывод результатов.
 
     // Упражнение 4: ввод количества слов и самих слов.
-    //int word_count = 0;
-    //if (!(cin >> word_count)) return 0;
+    int word_count = 0;
+    if (!(cin >> word_count)) return 0;
 
-    //vector<string> words;
-    //words.reserve(word_count);
-    //for (int i = 0; i < word_count; ++i) {
-    //    string word;
-    //    cin >> word;
-    //    words.push_back(word);
-    //}
+    vector<string> words;
+    words.reserve(word_count);
+    for (int i = 0; i < word_count; ++i) {
+        string word;
+        cin >> word;
+        words.push_back(word);
+    }
+    for (int i = 0; i < word_count; ++i) cout << words[i] << endl;
 
 
     int n;
@@ -32,10 +49,15 @@ int main() {
     cin >> n;
     if (n > 20) n = 20;
 
-    char words[20][11];
+    char** str_mass; // ** - для двумерного массива
+    str_mass = new char*[n]; // выделено ячеек под указатели на символы
+    for (int i = 0; i < n; i++)
+        str_mass[i] = new char[10]; // для каждой строки (i) выделено 10 символов
+
+    //char words[20][11];
 
     cout << "Введите слова:" << endl;
-    for (int i = 0; i < n; ++i) cin >> words[i];
+    for (int i = 0; i < n; ++i) cin >> str_mass[i];
 
     cout << "Слова с четными номерами (2, 4, ...):" << endl;
     for (int i = 1; i < n; i += 2) cout << words[i] << endl;
