81abe67e7460a29a5bd19f3d623919685dde793b
Dork <sabinamor4@gmail.com>
2026-03-31 22:50:11 +0700

    набросок решение системы линейных уравнений с вводом из консоли

--- labs/lab11_io/Lab11/src/main.cpp
+++ labs/lab11_io/Lab11/src/main.cpp
@@ -1,5 +1,4 @@
 
-
 // Lab 11
 // TODO: реализуйте решение по заданию в labs/lab11_io/README.md
 //
@@ -42,51 +41,56 @@
 // оставляет только целую часть и формат int
 //-удаляет содержимое существующего файла, размер становится нулевым
 
-//#include <iostream>
-//#include <fstream>
-//#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+//#define _CRT_SECURE_NO_WARNINGS
 //#include <cstdio>
-//#include <fcntl.h>
-//#include <unistd.h>
-//using namespace std;
-//int main() {
-//    string name;
-//
-//    int a;
-//    cout << "Input A integer number: ";
-//    cin >> a;
-//    cout << "We scored in your number A: "<< a<<endl;
-//
-//    int b;
-//    cout << "Please input your authentication password: ";
-//    scanf_s("%d", &b);
-//    printf_s("\nWe don't have a dataset...\nSorry, we will cheak it in future and save your new password: %d\n", b);
-//    char ch[5]={'e','r','y','j','f'};
-//    gets(ch);
-//    cout << ch;
-//    puts("hello");
-//
-//    //create object of class with name "fout" 
-//    // and merge/conjuction with name in "";
-//    ofstream fout("first.txt", ios::trunc);
-//    fout << "We are plenty\nAnd can do all in the world if believe in it";
-//    fout.close();
-//
-//    ifstream fin("first.txt");
-//    if (!fin.is_open()) cout << "File cannot open\n";
-//    else {
-//        char c;
-//        while (fin.get(c)) cout << c;
-//    }
-//
+#include <fcntl.h>
+#include <unistd.h>
+//#include "../../../../../../../../../../Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_ostream.hpp"
+using namespace std;
+int main() {
+    string name;
+
+    int a;
+    cout << "Input A integer number: ";
+    cin >> a;
+    cout << "We scored in your number A: "<< a<<endl;
+
+    int b;
+    cout << "Please input your authentication password: ";
+    scanf_s("%d", &b);
+    printf_s("\nWe don't have a dataset...\nSorry, we will cheak it in future and save your new password: %d\n", b);
+    char ch[5]={'e','r','y','j','f'};
+    char* str1 = new char[50];
+    //gets(str1);
+    cout << ch;
+    puts("hello");
+
+    //create object of class with name "fout" 
+    // and merge/conjuction with name in "";
+    ofstream fout("first.txt", ios::trunc);
+    fout << "We are plenty\nAnd can do all in the world if believe in it";
+    fout.close();
+
+    ifstream fin("first.txt");
+    if (!fin.is_open()) cout << "File cannot open\n";
+    else {
+        char c;
+        while (fin.get(c)) cout << c;
+    }
+
 
     FILE *fp;
-    int c,e;
-    if ((fp = fopen("first", "w")) != NULL) {
+    int c=4,e;
+    if ((int fp = fopen("first", "w")) != NULL) {
         for (int i = 0; i < 6; i++) {
             e = fscanf(fp, "%d\n", i);
         }
-        char* text = "Solo";
+        int fp = fopen("first", "w");
+        string text = "Solo";
         write(fp, text, 4);
         read(fp, text, 2);
     }
@@ -122,27 +126,27 @@
 // 
 //структура с 1 или 2 мерным массивом для запоминания корневого слова 
 
-#include <iostream>
-using namespace std;
-
-int main() {
-        file* f0, * f1, * f2; int n, i, k, s;
-
-        char* s0 = "in.txt", * s1 = "out.txt", * s2 = "out.txt";
-        if ((f0 = fopen(s0, "r")) == null ||
-            (f1 = fopen(s1, "w")) == null ||
-            (f2 = fopen(s2, "w")) == null) puts("ошибка!\n");
-        else
-        {
-            цикл
-            n = 0; s = 0;
-            while (feof(f0) == 0)
-            {
-                fscanf(f0, "%s", &k); s += k; n++;
-            }
-            fprintf(f2, "%d   %8.3f\n", n);
-        }
-
-    fclose(f0); fclose(f1); fclose(f2);
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//        file* f0, * f1, * f2; int n, i, k, s;
+//
+//        char* s0 = "in.txt", * s1 = "out.txt", * s2 = "out.txt";
+//        if ((f0 = fopen(s0, "r")) == null ||
+//            (f1 = fopen(s1, "w")) == null ||
+//            (f2 = fopen(s2, "w")) == null) puts("ошибка!\n");
+//        else
+//        {
+//            цикл
+//            n = 0; s = 0;
+//            while (feof(f0) == 0)
+//            {
+//                fscanf(f0, "%s", &k); s += k; n++;
+//            }
+//            fprintf(f2, "%d   %8.3f\n", n);
+//        }
+//
+//    fclose(f0); fclose(f1); fclose(f2);
+//    return 0;
+//}
--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 // Lab 13
 // TODO: реализуйте решение по заданию в labs/lab13_linear_algebra/README.md
@@ -8,7 +9,54 @@
 // - числа через пробел
 // - если несколько строк — в фиксированном порядке
 //asadasdasd
+double eps = 0.000001;
+int SystemOfLinearEquations(int n, float** A, float *X) {
+    int i,j,v,k;
+    float z,c ;
+    for (i = 0; i < n - 1; ++i) {
+        v = i;
+        for (j = i + 1; j < n; ++j)
+            if (abs(A[j][i]) > abs(A[v][i])) v = j;
+        if (abs(A[v][i]) < eps) return 0;
+        else {
+            for (j = i; j <= n; ++j) {
+                z = A[i][j]; A[i][j] = A[v][j]; A[v][j] = z;
+            }
+            for (k = i + 1; k < n; ++k) {
+                c = A[k][i] / A[i][i];
+                for (j = i; j <= n; ++j) {
+                    A[k][j] -= c * A[i][j];
+                }
+            }
+        }
+    }
+    for (i = n - 1; i >= 1; --i) {
+        for (k = 0; k < i - 1; ++k) {
+            c = A[k][i] / A[i][i];
+            A[k][n] -= c * A[i][n];
+            A[k][i] = 0;
+        }
+    }
+    for (i = 0; i < n; ++i) {
+        X[i] = A[i][n] / A[i][i];
+    }
+    return 1;
+}
+
 int main() {
-    // TODO
-    return 0;
+    int i, j, g, n; 
+    float** A, * X;
+    scanf_s("%d", &n);
+    X = new float[n];
+    A = new float*[n];
+    for (i = 0; i < n; ++i) {
+        A[i] = new float[n + 1];
+        for (j = 0; j <= n; ++j) scanf_s("%f", &A[i][j]);
+    }
+    g=SystemOfLinearEquations(n,A,X);
+    if (g) {
+        for (i = 0; i < n; ++i) printf("%8.3f", X[i]);
+        printf("\n");
+    }
+    else printf("ERROR\n");
 }
