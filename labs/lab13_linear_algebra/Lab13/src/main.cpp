50005e47c893c5076ac7a88401b6128880932759
Dork <sabinamor4@gmail.com>
2026-04-01 21:54:03 +0700

    решение слау методом Гаусса-Жордана  n  х n

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+using namespace std;
 
 // Lab 13
 // TODO: реализуйте решение по заданию в labs/lab13_linear_algebra/README.md
@@ -10,53 +11,73 @@
 // - если несколько строк — в фиксированном порядке
 //asadasdasd
 double eps = 0.000001;
-int SystemOfLinearEquations(int n, float** A, float *X) {
+int SystemOfLinearEquations(int M, int n, double** A, double*X) {
     int i,j,v,k;
-    float z,c ;
-    for (i = 0; i < n - 1; ++i) {
+    double z,c;
+    for (i = 0; i < M - 1; ++i) {
         v = i;
-        for (j = i + 1; j < n; ++j)
+        for (j = i + 1; j < M; ++j)
             if (abs(A[j][i]) > abs(A[v][i])) v = j;
         if (abs(A[v][i]) < eps) return 0;
         else {
-            for (j = i; j <= n; ++j) {
-                z = A[i][j]; A[i][j] = A[v][j]; A[v][j] = z;
-            }
-            for (k = i + 1; k < n; ++k) {
+            if (v!=i)
+                for (j = i; j <= n; ++j)
+                    z = A[i][j]; A[i][j] = A[v][j]; A[v][j] = z;
+            for (k = i + 1; k < M; ++k) {
                 c = A[k][i] / A[i][i];
-                for (j = i; j <= n; ++j) {
+                for (j = i; j <= n; ++j) 
                     A[k][j] -= c * A[i][j];
-                }
             }
         }
     }
-    for (i = n - 1; i >= 1; --i) {
+
+    cout << endl;
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cout << A[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+   
+    for (i = M - 1; i >= 1; --i) {
         for (k = 0; k < i - 1; ++k) {
             c = A[k][i] / A[i][i];
             A[k][n] -= c * A[i][n];
             A[k][i] = 0;
         }
     }
+
+    cout << endl;
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cout << A[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+
     for (i = 0; i < n; ++i) {
         X[i] = A[i][n] / A[i][i];
     }
     return 1;
 }
 
-int main() {
-    int i, j, g, n; 
-    float** A, * X;
-    scanf_s("%d", &n);
-    X = new float[n];
-    A = new float*[n];
-    for (i = 0; i < n; ++i) {
-        A[i] = new float[n + 1];
-        for (j = 0; j <= n; ++j) scanf_s("%f", &A[i][j]);
+void main() {
+    int i, j, g, n, M;
+    double** A, * X;
+    cin>>M;
+    cin>>n;
+    X = new double[n];
+    A = new double*[M];
+    for (i = 0; i < M; ++i) {
+        A[i] = new double[n + 1];
+        for (j = 0; j <= n; ++j) scanf_s("%lf", &A[i][j]);
     }
-    g=SystemOfLinearEquations(n,A,X);
+    g=SystemOfLinearEquations(M,n,A,X);
     if (g) {
-        for (i = 0; i < n; ++i) printf("%8.3f", X[i]);
+        for (i = 0; i < n; ++i) printf("%8.3lf", X[i]);
         printf("\n");
     }
-    else printf("ERROR\n");
+    else printf("Inconsistent system\n");
 }
