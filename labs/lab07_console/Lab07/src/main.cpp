b92e6e5c788627a816eeb112ff238f98c9563f84
Genius <sabinamor4@gmail.com>
2026-06-07 00:29:51 +0700

    pushy лабу, которую писала в лично созданном проекте

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -1,32 +1,67 @@
 #include <iostream>
+#include <math.h>
+#include <windows.h>
+using namespace std;
 
-// Lab 07
-// TODO: реализуйте решение по заданию в labs/lab07_console/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-int main() {
+//#include <bits/stdc++.h>
+
+int main()
+{
+    //float a = 0, b = 4, x, d = (b - a) / 9;
+    //double  radians, f;
+    //system("chcp 65001 > nul");
+    //cout << "введите начальную точку интервала возможных значений аргумента " << endl
+    //    << "x" << " " << "f" << endl;
+    ////cin >> x;
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    x = a + i * d;
+    //    radians = x * 3.1415926535 / 180;
+    //    f=sinl(radians) / x;
+    //    cout << x << " " << f << endl;// << a << b << d;
+    //}
+
     system("chcp 65001 > nul");
-    // Задание 1: вычисление значений функции.
-    // Ввод: x0 — начальная точка интервала.
-    double x0 = 0.0;
-    if (!(std::cin >> x0)) {
-        return 0;
-    }
-    // dfdfdf
-    // // dfd/df
-    // /dfdfd
-    // TODO: вычислите и выведите 10 значений функции на (0, 4].
+    int S = 0, N = 1, n, n0 = 0, n1 = 1, MAX;
+    cout << "введите MAX: ";
+    cin >> MAX;
 
-    // Задание 2: сумма чисел Фибоначчи.
-    // Ввод: MAX — ограничение на сумму.
-    long long max_value = 0;
-    if (!(std::cin >> max_value)) {
-        return 0;
+    for (int i = 1; n0 <= MAX; i++)
+    {
+        n = n0 + n1; n0 = n1; n1 = n;
+        if (n0 <= MAX)
+        {
+            cout << n0 << endl;
+            N = 1 + i;
+            S = S + n0;
+        }
     }
+    cout << N << " " << S;
 
-    // TODO: вычислите N и S (S <= MAX) и выведите результаты.
-    return 0;
+    //system("chcp 65001 > nul");
+    //float S, months, y, payment, mpayment, allper;
+
+    //printf("%u %4s %f", );
+    //scanf("% f", &S);
+    //printf(" %4 %f", );
+    //scanf("% f", &months);
+    //printf(" %4 %f", );
+    //scanf("% f", &y);
+
+    //printf(" %4 %f", );
+    //printf("  Долг Процент Платеж");
+    //printf(" %4 %f", );
+
+    //for (int i = 0; i <= months; i++)
+    //{
+    //    mpayment = S * y / (months * 100);
+    //    payment = S - mpayment;
+    //    printf(i \t S, mpayment, payment);
+    //    S = S - payment;
+    //    allper= allper+ mpayment;
+    //}
+
+    //printf(" %f /n", allper);
+
+
 }
